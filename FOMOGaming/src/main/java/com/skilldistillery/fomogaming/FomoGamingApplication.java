55ed4a2f86c9b26448caf78a90677ed5bf5c38d5
Ebony Johnson <ejohnson23451@gmail.com>
2021-12-10 19:03:44 -0500

    Spring Project added

---
+++ FOMOGaming/src/main/java/com/skilldistillery/fomogaming/FomoGamingApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.fomogaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FomoGamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FomoGamingApplication.class, args);
+	}
+
+}
---
+++ FOMOGaming/src/test/java/com/skilldistillery/fomogaming/FomoGamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.fomogaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FomoGamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
