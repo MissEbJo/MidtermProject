5cf923b73ed0d606480524e8edd74cfe5605adc3
Frederick Kaucher <frederick.kaucher@gmail.com>
2021-12-17 09:27:07 -0700

    created comment entity

---
+++ JPAFomoGaming/src/main/java/com/skilldistillery/fomogaming/entities/Comment.java
@@ -0,0 +1,65 @@
+package com.skilldistillery.fomogaming.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Comment {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String comment;
+
+	public int getId() {
+		return id;
+	}
+
+	public Comment() {
+		super();
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
+	@ManyToOne
+	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Comment other = (Comment) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "Comment [id=" + id + ", comment=" + comment + "]";
+	}
+
+}
